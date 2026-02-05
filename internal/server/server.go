0092aade3d65ac1ef2a06ce2a38e7e171eaac08e
Владыкин Никита <vld.kolkol@mail.ru>
2026-02-05 13:40:04 +0300

    first-iteration

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	HttpServer *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.RootHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	return &Server{HttpServer: &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+		ErrorLog: logger,
+		ReadTimeout: 5*time.Second,
+		WriteTimeout: 10*time.Second,
+		IdleTimeout: 15*time.Second,
+	},
+	Logger: logger,
+	}
+
+}
