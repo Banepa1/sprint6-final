1f6a87aaf1201c1a1b0c55099023e0896964639d
Владыкин Никита <vld.kolkol@mail.ru>
2026-02-05 14:21:00 +0300

    first-teration

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,60 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"time"
+	"path/filepath"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.ReadFile("./index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(file)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	rowData, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	preparedData := service.Converter(string(rowData))
+
+	logFile, err := os.Create(time.Now().UTC().Format("02-01-2006_15h04m05s") + filepath.Ext(handler.Filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer logFile.Close()
+
+	if _, err = logFile.Write([]byte(preparedData)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(preparedData))
+}
